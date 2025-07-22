ca6ddf620ecbdd2d19da040114c9a88f694e6731
AlterGhost44 <beniaminfirmowy1@outlook.com>
2025-07-22 19:08:32 +0200

    0.1.2

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 function popup() {
     document.getElementById("task").classList.toggle("hidden");
 }
+
+class task {
+    constructor(description, taskTime){
+        this.description = description;
+        this.taskTime = taskTime;
+        this.isDone = false;
+    }
+};
+
+const taskArray = [];
+
+function AddTask(){
+    const input_desc = document.getElementById("opis").value;
+    const input_time = document.getElementById("godzina").value;
+    const new_Task = new task(input_desc, input_time);
+    taskArray.push(new_Task);
+
+    const ul = document.getElementById("listaZadan");
+    ul.innerHTML = "";
+
+    for (t of taskArray) {
+        const li = document.createElement("li");
+         li.textContent = `${t.taskTime} - ${t.description} - Status: ${t.isDone}`;
+        ul.appendChild(li);
+    };
+    
+}
+
+function RemoveTask(){
+    for(t of taskArray) {
+        delete t;
+    }
+}
